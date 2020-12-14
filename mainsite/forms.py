dab578867a6a5126be95b9bbc5d1f9396625f52b
Nodir2001 <rasulovnd@stud.kai.ru>
2020-12-14 18:26:47 +0300

    new style for login and sign up

--- mainsite/forms.py
+++ mainsite/forms.py
@@ -4,15 +4,14 @@ from django.contrib.auth.models import User
 from .models import Sphere_of_life
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, help_text='Last Name')
-    last_name = forms.CharField(max_length=100, help_text='Last Name')
+    #first_name = forms.CharField(max_length=100, help_text='Last Name')
+    #last_name = forms.CharField(max_length=100, help_text='Last Name')
     email = forms.EmailField(max_length=150, help_text='Email')
 
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name',
-'email', 'password1', 'password2',)
+        fields = ('username','email', 'password1')
 
 
 
